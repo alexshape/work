bb8cfbdfbc2f54cc262d2ebed77e94dcea70ad59
alexshape <36480962+alexshape@users.noreply.github.com>
2023-01-25 09:52:09 +0300

    first

--- src/main/java/ru/sema/proj1/CrptApi.java
+++ src/main/java/ru/sema/proj1/CrptApi.java
@@ -1,4 +1,17 @@
 package ru.sema.proj1;
 
+import java.util.concurrent.TimeUnit;
+
 public class CrptApi {
+
+    private TimeUnit timeUnit;
+    private int requestLimit;
+
+
+    public CrptApi(TimeUnit timeUnit, int requestLimit) {
+        this.timeUnit = timeUnit;
+        this.requestLimit = requestLimit;
+    }
+
+
 }
